947ba0ac065481765047791d7183048d02ffcfd8
orrest <orrest@orrest.me>
2025-05-19 22:09:42 +0800

    mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'article-preview-component';
+  title =
+    'Shift the overall look and feel by adding these wonderful touches to furniture in your home';
+  content =
+    'Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.';
+  name = 'Michelle Appleton';
+  date = '28 Jun 2020';
+
+  showShare = signal(false);
+
+  onShareClick() {
+    this.showShare.set(!this.showShare());
+  }
 }
